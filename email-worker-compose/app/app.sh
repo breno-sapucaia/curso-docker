2e32a54c9548a7b5fc1eff222e14936618eda538
Breno <brenoss.magalhaes@outlook.com>
2019-08-20 22:43:00 -0300

    erro no sender

---
+++ email-worker-compose/app/app.sh
@@ -0,0 +1,3 @@
+pip install --upgrade pip
+python3 -m pip install bottle==0.12.13
+python3 -u sender.py
---
+++ email-worker-compose/app/sender.py
@@ -0,0 +1,11 @@
+from bottle import route,run,request
+
+
+@route('/', method='POST')
+def send():
+    assunto = request.forms.get('assunto')
+    mensagem = request.forms.get('mensagem')
+    return 'Mensagem enfileirada ! assunto: {} Mensagem: {}'.format(assunto, mensagem)
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8001, debug=True)
