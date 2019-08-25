2f51db243f15d61c5b2566e47fa9367c57d00fcc
Breno <brenoss.magalhaes@outlook.com>
2019-08-25 20:12:59 -0300

    curso finalizado

--- email-worker-compose/app/app.sh
+++ email-worker-compose/app/app.sh
@@ -1,3 +1,5 @@
-pip install --upgrade pip
-python3 -m pip install bottle==0.12.13
-python3 -u sender.py
+#!/bin/sh
+
+pip install bottle==0.12.13 redis==2.10.5
+pip install --no-binary :all: psycopg2==2.7.1
+python -u sender.py
